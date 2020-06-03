f65891bf788bcc57d29bdcd554c5f69f5b659244
CoryLHarris <sirrahyroc@gmail.com>
2020-06-03 16:36:43 -0500

    begin front end build for a random API to test connection

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './components/List.jsx';
+import NasaBlock from './components/NasaBlock.jsx';
+import Axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
+      items: [],
+      nasa: 42
     }
+    this.getPicOfDay = this.getPicOfDay.bind(this);
   }
 
   componentDidMount() {
-  //   $.ajax({
-  //     url: '/items',
-  //     success: (data) => {
-  //       this.setState({
-  //         items: data
-  //       })
-  //     },
-  //     error: (err) => {
-  //       console.log('err', err);
-  //     }
-  //   });
-  // }
+    this.getPicOfDay()
+  }
+
+  getPicOfDay(){
+    Axios.get('/picOfDay')
+    .then(response => {
+      console.log(response)
+    })
+    .catch(err => {
+      console.log(err, ' failed to get pic of the day in client')
+      this.setState({ nasa: response.data })
+    })
+  }
 
   render () {
     return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
+      <h1>Jeopardy</h1>
+      <NasaBlock data={this.state.nasa} />
     </div>)
   }
 }
